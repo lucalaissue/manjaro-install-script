d530acee5a6aea6d5b50fb5936d73bb1d316f09d
luca@laissue.ch <luca@laissue.ch>
2020-09-18 12:50:29 +0200

    Update scripts

--- auto-install.sh
+++ auto-install.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+# Download archive
 rm -rf /tmp/linux-install
 mkdir /tmp/linux-install
 cd /tmp/linux-install
-curl -L https://gitlab.com/lucalaissue/linux-config/-/archive/master/linux-config-master.tar.gz | tar -xz
-cd linux-config-master
-sh install.sh
+curl -fLo archive.zip https://github.com/lucalaissue/manjaro-install-script/archive/master.zip
+unzip archive.zip
+cd manjaro-install-script-master
+
+# Install
+cd manjaro
+sudo sh manjaro.sh $USER
+cd ../snap
+sudo sh install-snaps.sh
+cd ../vim
+sh install-vim.sh
+cd ..
+sh gnome/gnome.sh
+
+# Cleanup
 cd ~
 rm -r /tmp/linux-install
-
